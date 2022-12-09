b943d8c177d56eeb245ef954a9fcb2d873b5007a
Faez-B <faezbhatti@gmail.com>
2022-12-09 21:46:00 +0100

    pages -> new routes

--- app.vue
+++ app.vue
@@ -8,14 +8,16 @@
 <template>
   <div>
     
-    <div>
+    <!-- <div>
       <h1>Welcome to the homepage</h1>
       <AppAlert>
         This is an auto-imported component.
       </AppAlert>
-    </div>
+    </div> -->
 
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+
+    <NuxtPage />
 
   </div>
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+      <p>This page will be displayed at the /about route.</p>
+    </section>
+</template>
+  
