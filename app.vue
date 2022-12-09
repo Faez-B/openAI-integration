1ca736673ac6df2f5219a1df337610fe37858d1e
Faez-B <faezbhatti@gmail.com>
2022-12-09 21:42:07 +0100

    First component

--- app.vue
+++ app.vue
@@ -7,6 +7,15 @@
 
 <template>
   <div>
+    
+    <div>
+      <h1>Welcome to the homepage</h1>
+      <AppAlert>
+        This is an auto-imported component.
+      </AppAlert>
+    </div>
+
     <NuxtWelcome />
+
   </div>
 </template>
