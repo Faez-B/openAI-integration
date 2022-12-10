5d444247f6e0cdbd7622a8c8cfa452641ead1d41
Faez-B <faezbhatti@gmail.com>
2022-12-10 12:47:39 +0100

    Folder containing the details page should have singular name

---
+++ pages/post/[id].vue
@@ -0,0 +1,12 @@
+<script setup>
+    // import { useRoute } from 'vue-router'
+    const route = useRoute()
+    const { id } = route.params
+    console.log(id)
+</script>
+
+<template>
+    <h1>
+        Post {{ id }}
+    </h1>
+</template>
