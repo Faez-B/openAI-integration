0f9ebf5c4f1b20adf518899cab45b916efe5586c
Faez-B <faezbhatti@gmail.com>
2022-12-10 12:40:24 +0100

    Installation of axios module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
         apiSecret: '123',
         // Keys within public are also exposed client-side
         public: {
-          apiBase: '/api'
+          apiBase: 'https://jsonplaceholder.typicode.com/'
         }
     },
 
