78d6c514cd6f28ff561a7e45dd750a48238c2886
Faez-B <faezbhatti@gmail.com>
2022-12-10 12:50:58 +0100

    Slight improvement

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -6,6 +6,7 @@
     <header>
         <nav>
             <ul>
+                <li><NuxtLink to="/">Index</NuxtLink></li>
                 <li><NuxtLink to="/about">About</NuxtLink></li>
                 <li><NuxtLink to="/post/1">Post 1</NuxtLink></li>
                 <li><NuxtLink to="/post/2">Post 2</NuxtLink></li>
--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,8 +1,6 @@
 <script setup>
-    // import { useRoute } from 'vue-router'
     const route = useRoute()
     const { id } = route.params
-    console.log(id)
 </script>
 
 <template>
