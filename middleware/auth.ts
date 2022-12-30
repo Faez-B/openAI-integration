2b0d5a4e0e753b90f1dd04ba3c0b2a5b98e69cd9
Faez-B <faezbhatti@gmail.com>
2022-12-30 21:15:18 +0100

    Auth beginning

---
+++ middleware/auth.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    // isAuthenticated() is an example method verifying if an user is authenticated
+    // if (isAuthenticated() === false) {
+    //   return navigateTo('/login')
+    // }
+})
+  
