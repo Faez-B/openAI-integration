c1ed60304617a2cb44cd97b4e96ee752f7e732af
Faez-B <faezbhatti@gmail.com>
2022-12-30 22:08:56 +0100

    Interesting use cases for the API

---
+++ pages/openAI/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>
+        Veuillez choisir l'application à utiliser.
+    </h1>
+</template>
+
+<style scoped>
+    h1 {
+        height: 90vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
