85989cc807d03ccd8b5cb8ced0a83ab7e550111c
Faez-B <faezbhatti@gmail.com>
2022-12-30 23:04:18 +0100

    Sidebar links

--- components/OpenAISideBar.vue
+++ components/OpenAISideBar.vue
@@ -13,32 +13,32 @@
         <hr>
 
         <ul class="nav nav-pills flex-column mb-auto">
-          <li class="nav-item">
-            <NuxtLink to="/openai/q_a" class="nav-link d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                <i class="bi bi-patch-question pe-none me-2"></i>
-                <span class="fs-4">Q&A</span>
-            </NuxtLink>
-          </li>
+            <OpenAISidebarLink link="q_a" icon="patch-question" title="Q&A"/>
+            <OpenAISidebarLink link="chat" icon="chat-dots" title="Chat"/>
+            <OpenAISidebarLink link="friend" icon="emoji-smile" title="Friend chat"/>
+            <OpenAISidebarLink link="marv" icon="emoji-expressionless" title="Marv"/>
+            <OpenAISidebarLink link="facts" icon="lightbulb" title="Facts"/>
+            <OpenAISidebarLink link="englishTranslator" icon="globe-americas" title="Translator"/>
+            <OpenAISidebarLink link="interview" icon="question-diamond" title="Interview questions"/>
+            <OpenAISidebarLink link="essayOutline" icon="book" title="Essay outline"/>
+            <OpenAISidebarLink link="sql" icon="database-add" title="Create SQL requests"/>
+            <OpenAISidebarLink link="sql2" icon="database" title="Translate to SQL"/>
+            <OpenAISidebarLink link="adGeneration" icon="badge-ad" title="Ad Generation"/>
+            <OpenAISidebarLink link="codeTranslation" icon="code" title="Code translator"/>
+            <OpenAISidebarLink link="explainCode" icon="file-earmark-code" title="Explain code"/>
+            <OpenAISidebarLink link="jsHelper" icon="filetype-js" title="JavaScript helper"/>
+            <OpenAISidebarLink link="jsPython" icon="filetype-py" title="JS to Python"/>
+            <OpenAISidebarLink link="keywords" icon="text-wrap" title="Extract keywords"/>
+            <OpenAISidebarLink link="notesSummary" icon="card-text" title="Generate summary"/>
+            <OpenAISidebarLink link="studyNotes" icon="list-ol" title="Notes to study"/>
         </ul>
 
     </div>
 </template>
 
 <style scoped>
-    .nav-link {
-        border-radius: 10px;
-    }
-
-    .router-link-active.router-link-exact-active:not(.logo-link) {
-        color: rgba(33,37,41,var(--bs-bg-opacity,1)) !important;
-        background-color: white !important;
-    }
-
     .sidebar {
-        height: 100vh;
-    }
-
-    i {
-        font-size: 1.5rem;
+        max-height: 94vh;
+        overflow: auto;
     }
 </style>
---
+++ components/OpenAISidebarLink.vue
@@ -0,0 +1,42 @@
+<script setup>
+    const props = defineProps({
+        link: String,
+        icon: String,
+        title: String
+    });
+
+    const link = "/openai/" + props.link;
+    const icon = "bi-" + props.icon;
+    const { title } = props; 
+
+</script>
+
+<template>
+    <li class="nav-item">
+        <NuxtLink :to=link class="nav-link d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <i class="bi pe-none me-2" :class="icon"></i>
+            <span class="fs-6">{{ title }}</span>
+        </NuxtLink>
+    </li>
+</template>
+
+<style scoped>
+    .nav-link {
+        border-radius: 10px;
+    }
+
+    .router-link-active.router-link-exact-active:not(.logo-link) {
+        color: rgba(33,37,41,var(--bs-bg-opacity,1)) !important;
+        background-color: white !important;
+    }
+
+    .sidebar {
+        height: 100vh;
+        overflow: auto;
+    }
+
+    i {
+        font-size: 1.5rem;
+    }
+</style>
+
