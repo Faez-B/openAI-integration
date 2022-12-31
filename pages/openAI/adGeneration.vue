865d41885fcdb93e7d139132371491c7c9afd7a3
Faez-B <faezbhatti@gmail.com>
2022-12-31 20:42:21 +0100

    Instructions for Ad generation

--- pages/openAI/adGeneration.vue
+++ pages/openAI/adGeneration.vue
@@ -1,3 +1,32 @@
 <template>
-    <OpenAIForm link="ad-generation" />
+    
+    <div class="row">
+        <div class="col-12 col-md-5 order-3 order-md-1">
+            <OpenAIForm link="ad-generation" />
+        </div>
+
+        <div class="col-1 d-none d-md-block text-center order-md-2">
+            <div class="vr h-100 my-4"></div>
+        </div>
+
+        <div class="col-12 d-block d-md-none text-center order-2">
+            <hr class="w-100 mx-auto" />
+        </div>
+
+        <div class="col-12 col-md-6 order-1 order-md-3">
+            <h5 class="mt-3 text-secondary">
+                Turn a product description into ad copy.
+            </h5>
+        
+            <h2>
+                Example :
+            </h2>
+        
+            <div class="p-3 bg-light border rounded-3">
+                Write a creative ad for the following product to run on Facebook aimed at parents:
+
+                Product: Learning Room is a virtual environment to help students from kindergarten to high school excel in school.
+            </div>
+        </div>
+    </div>
 </template>
